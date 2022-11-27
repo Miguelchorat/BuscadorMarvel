8e21f8e271745009706ac9e013eb89dc47c4f0b7
Miguelchorat <miguelchorat@gmail.com>
2022-11-27 22:59:29 +0100

    Actualización index.html responsive. Añadido menu desplegable y eventos. Cambios en información.html para las tarjetas.

---
+++ dist/informacion.1e4a0065.js
@@ -0,0 +1,19 @@
+let encabezado_menu = document.querySelector(".encabezado__menu");
+let desplegable_cerrar = document.querySelector(".desplegable__cerrar");
+let fondo_desplegable = document.querySelector(".fondo__desplegable");
+let desplegable = document.querySelector(".desplegable");
+function abrirMenu() {
+    desplegable.style.right = 0;
+    //desplegable.style.setProperty('display', 'flex');
+    fondo_desplegable.style.setProperty("display", "block");
+}
+function cerrarMenu() {
+    desplegable.style.right = "-20vw";
+    //desplegable.style.setProperty('display', 'none');
+    fondo_desplegable.style.setProperty("display", "none");
+}
+encabezado_menu.addEventListener("click", abrirMenu);
+desplegable_cerrar.addEventListener("click", cerrarMenu);
+fondo_desplegable.addEventListener("click", cerrarMenu);
+
+//# sourceMappingURL=informacion.1e4a0065.js.map
